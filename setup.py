0fbb97f06f22d12d775b86d67499574cf50097af
Fábio Madeira <fbiomadeira@gmail.com>
2015-11-11 18:50:04 +0000

    Added setup.py. Should work out of the box.

---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    pyPDBeREST: A wrapper for the PDBe REST API.
+    Copyright (C) 2015  Fábio Madeira
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+from setuptools import setup
+from setuptools import find_packages
+
+__author__ = 'Fábio Madeira'
+__email__ = 'fabiomadeira@me.com'
+__version__ = '0.1.0'
+
+setup(
+    # Basic package information.
+    name='pypdberest',
+    version=__version__,
+    packages=find_packages(),
+
+    # Packaging options.
+    include_package_data=True,
+
+    # Package dependencies.
+    install_requires=['requests>=2.7.0'],
+
+    # Metadata for PyPI.
+    author=__author__,
+    author_email=__email__,
+    license='MIT',
+    url='http://github.com/biomadeira/pypdberest/tree/master',
+    keywords='pdbe rest api json python',
+    description='A python wrapper for the PDBe REST API.',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Bio-informatics',
+        'Topic :: Internet',
+        'Topic :: REST API',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
+
